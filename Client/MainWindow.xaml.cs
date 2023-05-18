1b57e7e3ba1721f6c921eed5a8d63c8c40fd9a5f
AndreyErin <eaa.forwork@gmail.com>
2023-05-18 14:17:45 +0700

    в клиентском приложение создан класс TcpClient для подключения к серверу и управления танком, функция управления танком описана

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -1,28 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.IO;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Client.Model;
 
 namespace Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
+
     public partial class MainWindow : Window
     {
+        private TcpClient tcpClient;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private Key _moveKey = Key.None;//кнопка отслеживающая пследнее движение
+        private Key _lastKey = Key.None;//кнопка нажатая пользователем
+        //двигаем танк ------править
+        private void MainWin_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.W:
+                case Key.Up:
+                    //сообщение серверу
+                    tcpClient.KeyOfControlTank(Key.Up);
+                    _moveKey = e.Key;
+                    break;
+                case Key.S:
+                case Key.Down:
+                    tcpClient.KeyOfControlTank(Key.Down);
+                    _moveKey = e.Key;
+                    break;
+                case Key.A:
+                case Key.Left:
+                    tcpClient.KeyOfControlTank(Key.Left);
+                    _moveKey = e.Key;
+                    break;
+                case Key.D:
+                case Key.Right:
+                    tcpClient.KeyOfControlTank(Key.Right);
+                    _moveKey = e.Key;
+                    break;
+                case Key.Space: //стрельба
+                    if (cD == false)
+                    {
+                        tcpClient.KeyOfControlTank(Key.Space);
+                        Task.Factory.StartNew(CooldownFire);//запускаем откат в отдельном потоке
+                    }
+                    break;
+            }
+
+        }
+        //править
+        private void MainWin_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == _moveKey)//если кнопка движения была поднята то останавливаем танк
+                tcpClient.KeyOfControlTank(Key.S);
+
+            _lastKey = Key.None;
+        }
+        //описан
+        private void MainWin_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (_lastKey == e.Key) //проверяем нажата ли та же самая кнопка или другая
+            {
+                e.Handled = true;//если ктопка та же самая, то помечаем событие как обработанное
+            }
+            _lastKey = e.Key;
+        }
+
+        private bool cD = false;
+        //откат выстрела
+        private void CooldownFire()
+        {
+            cD = true;
+            Thread.Sleep(300);
+            cD = false;
+        }
+
+        private void MainWin_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainWin_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnOut_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRaundWin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRaundLose_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Client/Model/TcpClient.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Client.Model
+{
+    public class TcpClient
+    {
+        protected Socket clientSocket { get; set; }
+
+        //protected TcpClient() { }
+        public TcpClient()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            clientSocket = socket;
+
+            Connect();
+        }
+
+        //подключение к серверу
+        public async Task Connect()
+        {
+            try
+            {
+                await clientSocket.ConnectAsync("192.168.0.34", 7070);
+                Task.Factory.StartNew(() => { GetDataOfServer(); SetDataOfServer(); });
+            }
+            catch (System.Exception e)
+            {
+                MessageBox.Show("Не удалось подключиться к серверу\n" + e.Message);
+            }
+        }
+
+        //управление танком
+        public void KeyOfControlTank(Key key) 
+        {
+            byte[] data;
+            string command = "";
+
+            switch (key)
+            {
+                case Key.Up:
+                    command = "MOVEUP^";
+                    break;
+                case Key.Down:
+                    command = "MOVEDOWN^";
+                    break;
+                case Key.Left:
+                    command = "MOVELEFT^";
+                    break;
+                case Key.Right:
+                    command = "MOVERIGHT^";
+                    break;
+                case Key.S:
+                    command = "STOP^";
+                    break;
+
+                case Key.Space:
+                    command = "FIRE^";
+                    break;                    
+            }
+
+            data = Encoding.UTF8.GetBytes(command);
+            SetDataOfServer(data);
+        }
+
+
+        //отправить данные
+        private async Task SetDataOfServer(byte[] data)
+        {
+            await clientSocket.SendAsync(data, SocketFlags.None);
+        }
+
+        //получить данные
+        private async Task GetDataOfServer()
+        {
+            
+        }
+    }
+}
